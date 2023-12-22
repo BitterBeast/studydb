52a45763f03740ba024fbb9bd7579a6adf0a6155
BitterBeast <141508197+BitterBeast@users.noreply.github.com>
2023-12-22 19:46:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,13 @@
 from motor.motor_asyncio import AsynIOMotor
+from config import config
+
 
 class Database:
     def _init_(self, uri, database):
       self._client = AsyncIOMotor(uri)
       self.db = self._client[database_name]
       self.col = self.db.user
+
+
+
+db = Database(config.DB_URL, config.DB_NAME)
